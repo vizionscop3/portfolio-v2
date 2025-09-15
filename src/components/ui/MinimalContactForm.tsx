68c26195b215d538dc329b01f9803d031ab4f57e
Vizion <vizionscop3@gmail.com>
2025-09-15 13:00:39 -0400

    style: increase GET IN TOUCH font size to 36px (text-4xl) for better visual impact

--- src/components/ui/MinimalContactForm.tsx
+++ src/components/ui/MinimalContactForm.tsx
@@ -26,8 +26,8 @@ const MinimalContactForm: React.FC = () => {
 
   return (
     <div className="w-full max-w-md mx-auto p-6">
-      <h3 className="text-2xl mb-6 text-center tt-frantz-menu text-cyan-400">
-        Get In Touch
+      <h3 className="text-4xl mb-6 text-center tt-frantz-menu text-cyan-400">
+        GET IN TOUCH
       </h3>
 
       {isSubmitted ? (
